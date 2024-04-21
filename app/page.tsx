4c3288cd7bdc8440bd11653c01af861740234f1f
Noman Jallal <nomanjalaal@gmail.com>
2024-04-21 17:11:24 +0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+export default function Home() {
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-black">
+      <div className="space-y-6 text-center">
+        <h1 className={cn("text-3xl font-semibold text-white drop-shadow-md", font.className)}>
+          ✈ Airline Management System
+        </h1>
+        <p className="text-white text-lg drop-shadow-md">A simple database project</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              SignIn
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
+}
