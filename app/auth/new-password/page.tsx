6072eac24a29eb7834f5334dc5a23c0f42dee44c
Noman Jallal <nomanjalaal@gmail.com>
2024-04-26 16:33:10 +0500

    reset password form

---
+++ app/auth/new-password/page.tsx
@@ -0,0 +1,7 @@
+import { NewPasswordForm } from "@/components/auth/new-password-form";
+
+const Page = () => {
+  return <NewPasswordForm />;
+};
+
+export default Page;
---
+++ components/auth/new-password-form.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { NewPasswordSchema } from "@/schemas";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { FormError } from "@/components/form-error";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { FormSuccess } from "@/components/form-success";
+import { newPassword } from "@/actions/new-password";
+import { useTransition, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+export const NewPasswordForm = () => {
+  const [isPending, startTransition] = useTransition();
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const form = useForm<z.infer<typeof NewPasswordSchema>>({
+    resolver: zodResolver(NewPasswordSchema),
+    defaultValues: {
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof NewPasswordSchema>) => {
+    setError("");
+    setSuccess("");
+
+    startTransition(() => {
+      newPassword(values, token).then((data) => {
+        setError(data?.error);
+        setSuccess(data?.success);
+      });
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLabel="Enter a new password!"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="******" type="password" disabled={isPending} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button type="submit" className="w-full" disabled={isPending}>
+            Rest password
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
